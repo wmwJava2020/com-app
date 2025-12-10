b61b49cc179417d128000f6048ff4ffa26c94ba4
AIDA <meandaida@gmail.com>
2025-12-10 14:47:48 -0600

    second commit

---
+++ zemaproduction/src/main/java/com/zemaproduction/entity/ZemaEntity.java
@@ -0,0 +1,16 @@
+/**
+ * Created by:AIDA
+ * Date : 12/10/2025
+ * Time : 2:35 PM
+ */
+package com.zemaproduction.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class ZemaEntity {
+
+    @Id
+    String itemId;
+}
